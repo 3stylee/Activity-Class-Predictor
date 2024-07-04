f37ea7d34818286f9aec61d767fd888bfefe0af4
Lewis Watt <L.Watt-21@student.lboro.ac.uk>
2024-07-04 10:41:54 +0100

    working now

--- predictData.js
+++ predictData.js
@@ -11,7 +11,7 @@ const ACTIVITY_LABEL_MAPPING = {
 /**
  * Feeds an array of objects representing activities through a Neural Network. Returns an array of predicted activity types (tempo, long run etc...).
  *
- * @param {Object[]} data - An array of objects, each representing an activity.
+ * @param {number[][]} data - An array of objects, each representing an activity.
  *
  * @returns {String[]} - An array of strings, each representing a predicted activity type.
  */
@@ -21,11 +21,12 @@ const predictData = async (data) => {
 		"https://raw.githubusercontent.com/3stylee/Track-it/master/src/constants/tfjs_model/model.json"
 	)
 
-	for (row in data) {
+	for (const row of data) {
 		if (row[6] !== "Run") {
 			results.push(row[6])
 		} else {
-			const tensorData = tf.tensor2d([row.slice(0, 5)])
+			row.pop()
+			const tensorData = tf.tensor2d([row])
 			const prediction = model.predict(tensorData)
 			const predictedClassIndex = prediction.argMax(1).dataSync()[0]
 			results.push(ACTIVITY_LABEL_MAPPING[predictedClassIndex])
